97ab67502184c2782f414b7d1ce8bbe317e71ce5
gogiwung <154584654+gogiwung@users.noreply.github.com>
2024-01-09 14:03:33 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import os
 import openai
 # from dotenv import load_dotenv
-from langchain_openai import ChatOpenAI
+from langchain.chat_models import ChatOpenAI
 import streamlit as st
 import time
 
 # load_dotenv()
-api_key = os.environ.get('OPENAI_API_KEY')
-chat_model = ChatOpenAI(openai_api_key=api_key)
+# api_key = os.environ.get('OPENAI_API_KEY')
+# api_key = os.getenv('OPENAI_API_KEY')
+# chat_model = ChatOpenAI(openai_api_key=api_key)
+chat_model = ChatOpenAI()
 
 st.title('시를 써보자')
 st.title('_Streamlit_ is :blue[cool] :sunglasses:')
@@ -16,9 +18,12 @@ st.title('_Streamlit_ is :blue[cool] :sunglasses:')
 title = st.text_input('어떤 내용의 시를 써줄까?')
 st.write( title, '에 대한 시를 써줄게')
 
+# result = chat_model.predict(title + "에 대한 시를 써줘")
+# st.write(result)
 
 if st.button('시 작성'):
     with st.spinner('Wait for it...'):
+
         result = chat_model.predict(title + "에 대한 시를 써줘")
         st.write(result)
 
